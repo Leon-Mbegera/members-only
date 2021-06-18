b60efc44cfab9723dacd643dcf585cd0983d7140
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-06-18 16:19:16 +0300

    Make create action to build a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
 
   def new
-    @posts = Posts.new
+    @post = Posts.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.valid? && @posts.save
+      redirect_to posts_url
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  
+  end
+  
+  
 end
